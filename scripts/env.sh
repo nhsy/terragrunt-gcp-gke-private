2575743437ca917053d297be728faf2bb40118a8
nhsy <46715264+nhsy@users.noreply.github.com>
2021-11-04 09:19:02 +0000

    Added TG_ENV variable

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,9 +3,16 @@
 export TG_PROJECT=$(jq -r '.project_id' ./resources/common_vars.json)
 export TG_REGION=$(jq -r '.region' ./resources/common_vars.json)
 
+if [[ -z "$TG_ENV" ]]; then
+  export TG_ENV=dev
+fi
+
 gcloud config set project $TG_PROJECT
 gcloud config list
 
-export TG_BUCKET=terraform-state-$(gcloud projects describe $TG_PROJECT --format="value(projectNumber)")
+export TG_BUCKET=terraform-state-$(gcloud projects describe $TG_PROJECT --format="value(projectNumber)")-$TG_ENV
+
+echo "TG environment variables:"
+env | grep "TG_"
 
 alias kp='HTTPS_PROXY=localhost:8080 kubectl $*'
